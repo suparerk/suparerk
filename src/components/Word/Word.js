75cbf19f7de35c773db1fe4ca6542c0d3cd3949c
suparerk <ch.suparerk@gmail.com>
2017-03-23 18:17:25 +0700

    handleInput method

--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -8,24 +8,29 @@ const propTypes = {
 }
 
 
-const Word = ({ input, submit }) => (
-  <div className="App w3-content w3-padding-128">
-    <div className="w3-row-padding">
-      <div className="w3-pink w3-container w3-third">
-        <h1>Your word here:</h1>
+const Word = ({ input, submit }) => {
+  const handleInput = (e) => {
+    console.log(e.target.value)
+    submit(e.target.value)
+  }
+  return (
+    <div className="App w3-content w3-padding-128">
+      <div className="w3-row-padding">
+        <div className="w3-pink w3-container w3-third">
+          <h1>Your word here:</h1>
+        </div>
+        <div className="w3-container w3-twothird">
+          <input
+            type="text"
+            className="w3-input w3-margin-top"
+            onChange={handleInput}
+          />
+        </div>
       </div>
-      <div className="w3-container w3-twothird">
-        <input
-          type="text"
-          className="w3-input w3-margin-top"
-          onKeyUp={submit}
-        />
-      </div>
+      <h2>{input}</h2>
     </div>
-    <h2>{input}</h2>
-  </div>
-)
-
+  )
+}
 Word.propTypes = propTypes
 
 
