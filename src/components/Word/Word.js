accb8ff57958fa3682b8844961d0966139fa7cd3
Suparerk <ch.suparerk@gmail.com>
2017-04-18 13:10:52 +0700

    cleanup

--- src/components/Word/Word.js
+++ src/components/Word/Word.js
@@ -1,14 +1,11 @@
 import React, { Component, PropTypes } from 'react'
 import Card from '../Card'
 import Slot from '../Slot'
-import map from 'lodash/map'
 
-const { array, bool, object } = PropTypes
+const { object } = PropTypes
 
 const propTypes = {
-  available: array.isRequired,
   cards: object.isRequired,
-  completed: bool,
 }
 
 const defaultProps = {
@@ -17,40 +14,30 @@ const defaultProps = {
 
 class Word extends Component {
   render() {
-    const { available, cards, completed, move, slots, position } = this.props
+    const { cards, move, slots, position } = this.props
     return (
       <div className="App w3-content w3-padding-128">
         {/* {JSON.stringify(completed)} */}
-        {/* <div className="flex">
-          {available.map(id =>
-            <Card
-              key={id}
-              {...cards[id]}
-            />)
-          }
-        </div> */}
         <hr />
         <div className="flex">
           {
             Object.keys(slots).map((id, index) => {
-            const slot = slots[id]
-            return (
-              <Slot
-                key={id}
-                {...slot}
-                onMove={({ sourceId, targetId }) =>
-                move(sourceId, targetId)}
-              >
-                <Card
-                  active={index < position}
-                  {...cards[slot.cardId]}
-                />
-                <div>
-
-                </div>
-              </Slot>
-            )
-          })}
+              const slot = slots[id]
+              return (
+                <Slot
+                  key={id}
+                  {...slot}
+                  onMove={({ sourceId, targetId }) =>
+                  move(sourceId, targetId)}
+                >
+                  <Card
+                    active={index < position}
+                    {...cards[slot.cardId]}
+                  />
+                </Slot>
+              )
+            })
+          }
         </div>
       </div>
     )
